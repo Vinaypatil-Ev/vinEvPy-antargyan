e573661381665dfac8197dc769e0003580ca06cf
Vinaypatil-Ev <vinaypatil153@gmail.com>
2020-08-23 12:22:05 +0530

    create tensorflow linear_regression.py

--- antarjnan/algos/pytorch/linear_regression.py
+++ antarjnan/algos/pytorch/linear_regression.py
@@ -5,5 +5,7 @@ class LinearRegress:
     def __init__(X,y):
         self.X=return_torch_tensor(X)
         self.y=return_torch_tensor(y)
-    
-    
+
+    def cal_X():
+        ones=torch.ones(self.X.size(0))
+        return X
