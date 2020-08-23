8c851a125de5364b5e4aadf67873f468691b65c1
Vinaypatil-Ev <vinaypatil153@gmail.com>
2020-08-23 15:30:44 +0530

    added version

---
+++ antarjnan/version.py
@@ -0,0 +1,10 @@
+version=(0,1,0)
+#version system
+#major.minor.minute
+
+def set_get_version():
+    print("%d.%d.%d" % version)
+    return "%d.%d.%d" % version
+
+    
+__version__=set_get_version()
--- setup.py
+++ setup.py
@@ -1,1 +1,9 @@
 from setuptools import setup
+
+with open README.md as readme:
+    long_description=readme.read()
+
+setup(
+    name="antarjyan",
+    
+)
