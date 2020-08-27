2954d4f554ed1e5459ec53b06a12b4646c7c1db5
Vinaypatil-Ev <vinaypatil153@gmail.com>
2020-08-27 22:55:46 +0530

    version 27aug20

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         'numpy',
         'torch>=1.5.0',
     ],
-    packages=find_packages()
+    packages=find_packages(),
     python_requires='>=3.6.1',
     classifiers=[
         'Programming Language :: Python :: 3.6',
