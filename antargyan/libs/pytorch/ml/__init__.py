7f75afabe60c38f563ded493a1ecceeb1800c0e2
Vinaypatil-Ev <vinaypatil153@gmail.com>
2020-10-18 17:00:10 +0530

    18 oct 2020

--- antargyan/libs/pytorch/ml/__init__.py
+++ antargyan/libs/pytorch/ml/__init__.py
@@ -2,4 +2,4 @@ from .reg.linear_regression import LinearRegression
 from .reg.k_nearest_neighbour import KNNReg
 from .classi.k_nearest_neighbour import KNNClassi
 
-__all__ = ['LinearRegression', "KNNReg", 'KKNClassi']
+__all__ = ['LinearRegression', "KNNReg", 'KNNClassi']
